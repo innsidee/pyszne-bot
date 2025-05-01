86a8fa7b164e1a69f6dfd170a40faa718232b5ea
innsidee <lpatrey8@gmail.com>
2025-05-01 16:56:55 +0200

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+import TelegramBot from 'node-telegram-bot-api';
 import express from 'express';
+import dotenv from 'dotenv';
+
 const app = express();
 const port = process.env.PORT || 3000;
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Siema benc!');
+});
 
 app.get('/', (req, res) => {
   res.send('Bot działa!');
